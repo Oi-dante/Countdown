9e6c859fdaaa82af2ad95e9999b8405be2730e57
Gustavo Barboza <gustavobarboza2003@gmail.com>
2022-12-13 14:31:06 -0300

    made page responsive and allow user to change countdown

--- script.js
+++ script.js
@@ -1,20 +1,50 @@
-const date = new Date("11/16/2022").getTime();
+// ### Ler sobre: Arrow function ###
 
-var timer = setInterval(function() {
-	const now = new Date().getTime();
-	const distance = (date - now);
+// Variável para armazenar ID do countdown
+let countdownId
 
-	const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-	const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-	const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-	const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+// Função que inicia o countdown
+const startCountdown = date => {
+	countdownId = setInterval(() => {
+		const now = new Date().getTime();
+		const distance = (date - now);
+		
+		const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+		const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+		const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+		const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+		
+		document.getElementById("days").innerHTML = days;
+		document.getElementById("hours").innerHTML = hours;
+		document.getElementById("minutes").innerHTML = minutes;
+		document.getElementById("seconds").innerHTML = seconds;
+		
+		if(distance <= 0) {
+			document.getElementById("timer").innerHTML = "The timer is gone"
+		}
+	}, 1000)
+}
+
+// Pegar a data do HTML
+const changeTimeBtn = document.getElementById('change-time')
+
+// Função que irá lidar com o click
+const handleClick = () => {
+	const countdownTimer = document.getElementById('calendar')
 	
-    document.getElementById("days").innerHTML = days;
-    document.getElementById("hours").innerHTML = hours;
-    document.getElementById("minutes").innerHTML = minutes;
-    document.getElementById("seconds").innerHTML = seconds;
+	// Transformar a data do HTML em uma data que podemos utilizar no JS
+	const date = new Date(countdownTimer.value).getTime()
+	
+	// Resetar countdown
+	clearInterval(countdownId)
 
-	if(distance <= 0) {
-		document.getElementById("timer").innerHTML = "The timer is gone"
-	}
-}, 1000);   
+	// Iniciar countdown
+	// ADICIONAR TRATATIVA DE DATA MENOR QUE A ATUA
+	
+	startCountdown(date)
+}
+
+
+
+// Evento => executar quando clicar no botão
+changeTimeBtn.addEventListener('click', handleClick)
